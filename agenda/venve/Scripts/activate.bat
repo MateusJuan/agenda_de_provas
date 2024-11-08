009691d981fa1cffb70cee9d0137af643e3b9716
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-08 18:42:12 -0300

    08/11/2024

--- agenda/venve/Scripts/activate.bat
+++ agenda/venve/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Aluno\Desktop\nam\agenda_de_provas\agenda\venve
+set VIRTUAL_ENV=C:\Users\Mateus Juan\Documents\GitHub\agenda_de_provas\venve
 
 if not defined PROMPT set PROMPT=$P$G
 
